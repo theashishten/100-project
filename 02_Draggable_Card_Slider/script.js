ce4e8cca0d8cc9472af7f776516deddbd7cbc9f8
Ashish <theashishten@gmail.com>
2025-02-24 07:10:28 +0530

    started scripting

--- 02_Draggable_Card_Slider/script.js
+++ 02_Draggable_Card_Slider/script.js
@@ -1,25 +1,28 @@
-const carousel = document.querySelector('.carousel');
+const carousel = document.querySelector(".carousel");
 
+let isDragging = false,
+  startX,
+  startScrollLeft;
 
-let isDragging = false ,startX, startScrollLeft;
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  console.log(carousel);
+  
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+  console.log(startScrollLeft);
+  
+};
 
-const dragStart = () =>{
-    isDragging = true
-    carousel.classList.add('dragging')
-    startX = e.pageX;
-    startScrollLeft =carousel.scrollLeft
-}
-
-const dragging = (e)=>{
-    if(!isDragging) return
-  carousel.scrollLeft = e.pageX;
-    
-}
-const dragStop = ()=>{
-  isDragging = false
-  carousel.classList.remove('dragging')
-
-}
-carousel.addEventListener("mousedown", dragStart)
-carousel.addEventListener("mousemove", dragging)
-document.addEventListener('mouseup',dragStop)
+const dragging = (e) => {
+  if (!isDragging) return;
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
